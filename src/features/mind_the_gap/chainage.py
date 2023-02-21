3b718458013c7c17d4486a0abfaaeb1691c87434
v7g <v7g@teahupoo.ornl.gov>
2023-02-21 15:12:04 -0500

    Chainage function can handle types

--- src/features/mind_the_gap/chainage.py
+++ src/features/mind_the_gap/chainage.py
@@ -4,6 +4,8 @@ import geopandas as gpd
 import numpy as np
 from shapely.ops import unary_union
 from shapely.geometry import MultiPoint
+from shapely.geometry import MultiLineString
+from shapely.geometry import LineString
 
 def chainage(boundary_line, interval, coord_sys='EPSG:4326'):
     """Generates a set of points at equal intervals along a line
@@ -22,16 +24,31 @@ def chainage(boundary_line, interval, coord_sys='EPSG:4326'):
     chain_points_ds : GeoSeries
 
     """
+    if isinstance(boundary_line, MultiLineString):
+        chain_points = MultiPoint()
+        for line in boundary_line.geoms:
+            distances = np.arange(0, line.length, interval)
+            points = [line.interpolate(distance) for distance in distances] + \
+                    [line.boundary]
+            points = unary_union(points)
+            chain_points = unary_union([chain_points, points])
 
-    chain_points = MultiPoint()
-    for line in boundary_line.geoms:
-        distances = np.arange(0, line.length, interval)
-        points = [line.interpolate(distance) for distance in distances] + \
-                [line.boundary]
+        chainage_ds = gpd.GeoSeries(chain_points, crs=coord_sys)
+        chainage_ds = chainage_ds.explode(ignore_index=True)
+
+        return chainage_ds
+
+    elif isinstance(boundary_line, LineString):
+        chain_points = MultiPoint()
+        distances = np.arange(0, boundary_line.length, interval)
+        points = [boundary_line.interpolate(distance) \
+            for distance in distances] + [boundary_line.boundary]
         points = unary_union(points)
         chain_points = unary_union([chain_points, points])
 
-    chainage_ds = gpd.GeoSeries(chain_points, crs=coord_sys)
-    chainage_ds = chainage_ds.explode(ignore_index=True)
+        chainage_ds = gpd.GeoSeries(chain_points, crs=coord_sys)
+        chainage_ds = chainage_ds.explode(ignore_index=True)
 
-    return chainage_ds
+        return chainage_ds
+    else:
+        raise TypeError("boundary_line mus be LineString or MultiLineString")
