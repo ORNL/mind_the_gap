f321ae3c99aead60ece680728d897947b01c2370
v7g <v7g@teahupoo.ornl.gov>
2024-07-23 13:30:01 -0400

    Test mind_the_gap.does_cross

--- src/tests/test_mind_the_gap.py
+++ src/tests/test_mind_the_gap.py
@@ -25,7 +25,7 @@ class TestMindTheGap:
         points = np.array([[1,2],[2,1],[3,4]])
         points_in_bins, y_bins, x_bins = \
             mind_the_gap.into_the_bins(points, x_bin_size=0.5, y_bin_size=0.5)
-        
+
         expexted_points_in_bins = np.array([[1,2,2,0],
                                             [2,1,0,2],
                                             [3,4,6,4]])
@@ -35,3 +35,21 @@ class TestMindTheGap:
         assert_array_equal(y_bins, expected_y_bins)
         assert_array_equal(x_bins, expected_x_bins)
         assert_array_equal(points_in_bins, expexted_points_in_bins)
+
+    def test_find_lat_gaps(self):
+        pass
+
+    def test_find_lon_gaps(self):
+        pass
+
+    def test_does_cross(self):
+        xg1 = [0, 1, 0, 1, 0, 3, 2]
+        yg1 = [0, 2, 0, 0, 0, 3, 3]
+
+        cross_1 = mind_the_gap.does_cross(xg1,yg1)
+
+        xg2 = [0, 1, 0, 4, 0, 5, 1]
+        yg2 = [0, 2, 0, 1, 1, 3, 2]
+        cross_2 = mind_the_gap.does_cross(xg2,yg2)
+
+        assert cross_1 is True and cross_2 is False
