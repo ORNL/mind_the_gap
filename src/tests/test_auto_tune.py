b3d4c0dcec96473d9c8a23937874c669cfc68053
v7g <v7g@teahupoo.ornl.gov>
2024-07-31 13:01:53 -0400

    Test Region.__init__ (just setting the boundaries attribute)

--- src/tests/test_auto_tune.py
+++ src/tests/test_auto_tune.py
@@ -11,6 +11,26 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from shapely.geometry import Point
 from shapely.geometry import LineString
+from shapely.testing import assert_geometries_equal
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from auto_tune import Region
+
+class TestRegion:
+    def setup_method(self):
+        self.points = gpd.read_file('./src/tests/data/test_points.geojson')
+        self.bound = gpd.read_file('./src/tests/data/test_bound.geojson')
+
+    def test_init(self):
+        reg = Region(self.points, self.bound)
+
+        exp_boundaries = LineString([(-1.2593125, 1.0945),
+                                     (-1.1593125, -0.7175),
+                                     (0.7886875, -0.7175),
+                                     (0.7886875, 1.0945),
+                                     (-1.2593125, 1.0945)])
+
+        assert_geometries_equal(reg.boundaries, exp_boundaries)
+
+    def test_make_grid(self):
+        pass
