52045a50470d13509f26de9331b39c738b8a2afc
v7g <v7g@teahupoo.ornl.gov>
2024-07-31 14:09:00 -0400

    Test Region.make_grid method

--- src/tests/test_auto_tune.py
+++ src/tests/test_auto_tune.py
@@ -21,6 +21,9 @@ class TestRegion:
         self.points = gpd.read_file('./src/tests/data/test_points.geojson')
         self.bound = gpd.read_file('./src/tests/data/test_bound.geojson')
 
+        self.exp_grid = gpd.read_file('./src/tests/data/exp_grid.geojson')
+        self.exp_grid = self.exp_grid.set_index('index').rename_axis(None)
+
     def test_init(self):
         reg = Region(self.points, self.bound)
 
@@ -33,4 +36,10 @@ class TestRegion:
         assert_geometries_equal(reg.boundaries, exp_boundaries)
 
     def test_make_grid(self):
-        pass
+        reg = Region(self.points, self.bound, grid_size = 0.02)
+        reg.make_grid(size = 0.02)
+
+        assert_geodataframe_equal(reg.grid,
+                                  self.exp_grid,
+                                  check_index_type=False,
+                                  check_less_precise=True)
