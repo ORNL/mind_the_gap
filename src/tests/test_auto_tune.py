66851b85883186ee7d78788cb951e7aefeda3a5b
v7g <v7g@teahupoo.ornl.gov>
2024-08-07 09:51:53 -0400

    Use fixtures for setting up all tests

--- src/tests/test_auto_tune.py
+++ src/tests/test_auto_tune.py
@@ -12,24 +12,50 @@ from numpy.testing import assert_array_equal
 from shapely.geometry import Point
 from shapely.geometry import LineString
 from shapely.testing import assert_geometries_equal
+import pytest
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from auto_tune import Region
 
+@pytest.fixture()
+def points():
+    _points = gpd.read_file('./src/tests/data/test_points.geojson')
+    return _points
+
+@pytest.fixture()
+def bound():
+    _bound = gpd.read_file('./src/tests/data/test_bound.geojson')
+    return _bound
+
+@pytest.fixture()
+def exp_grid():
+    _exp_grid = gpd.read_file('./src/tests/data/exp_grid.geojson')
+    _exp_grid = _exp_grid.set_index('index').rename_axis(None)
+    return _exp_grid
+
+@pytest.fixture()
+def exp_mind_gaps():
+    _exp_mind_gaps = gpd.read_file('./src/tests/data/exp_mind_gaps.gpkg')
+    return _exp_mind_gaps
+
+@pytest.fixture()
+def exp_auto_gaps():
+    _exp_auto_gaps = gpd.read_file('./src/tests/data/exp_auto_gaps.gpkg')
+    return _exp_auto_gaps
+
+@pytest.fixture()
+def exp_parallel_gaps():
+    _exp_parallel_gaps=gpd.read_file('./src/tests/data/exp_parallel_gaps.gpkg')
+    return _exp_parallel_gaps
+
 class TestRegion:
-    def setup_method(self):
-        self.points = gpd.read_file('./src/tests/data/test_points.geojson')
-        self.bound = gpd.read_file('./src/tests/data/test_bound.geojson')
+    @pytest.fixture(autouse=True)
+    def _test_points(self, points):
+        self.points = points
 
-        self.exp_grid = gpd.read_file('./src/tests/data/exp_grid.geojson')
-        self.exp_grid = self.exp_grid.set_index('index').rename_axis(None)
-
-        self.exp_mind_gaps = \
-            gpd.read_file('./src/tests/data/exp_mind_gaps.gpkg')
-        self.exp_auto_gaps = \
-            gpd.read_file('./src/tests/data/exp_auto_gaps.gpkg')
-        self.exp_parallel_gaps = \
-            gpd.read_file('./src/tests/data/exp_parallel_gaps.gpkg')
+    @pytest.fixture(autouse=True)
+    def _test_bound(self, bound):
+        self.bound = bound
 
     def test_init(self):
         reg = Region(self.points, self.bound)
@@ -42,19 +68,19 @@ class TestRegion:
 
         assert_geometries_equal(reg.boundaries, exp_boundaries)
 
-    def test_make_grid(self):
+    def test_make_grid(self, exp_grid):
         reg = Region(self.points, self.bound, grid_size = 0.02)
         reg.make_grid(size = 0.02)
 
         assert_geodataframe_equal(reg.grid,
-                                  self.exp_grid,
+                                  exp_grid,
                                   check_index_type=False,
                                   check_less_precise=True)
-    def test_mind(self):
+    def test_mind(self, exp_mind_gaps):
         reg = Region(self.points, self.bound)
         reg.mind(0.063, 2, 3, 18)
 
-        assert_geodataframe_equal(reg.gaps, self.exp_mind_gaps)
+        assert_geodataframe_equal(reg.gaps, exp_mind_gaps)
 
         reg.mind(-0.063, -2, -3, -18)
 
@@ -75,22 +101,22 @@ class TestRegion:
 
         assert fit is False
 
-    def test_run(self):
+    def test_run(self, exp_auto_gaps):
         reg = Region(self.points, self.bound, grid_size=0.05)
         reg.run(area_ceiling=0.5)
 
-        assert_geodataframe_equal(reg.gaps, self.exp_auto_gaps)
+        assert_geodataframe_equal(reg.gaps, exp_auto_gaps)
 
-    def test_parallel_run(self):
+    def test_parallel_run(self, exp_auto_gaps):
         reg = Region(self.points, self.bound, grid_size=0.05)
 
         reg.parallel_run(0.07,0.2,0.5,0.1,0.025,2,3,20)
 
-        assert_geodataframe_equal(reg.gaps, self.exp_auto_gaps)
+        assert_geodataframe_equal(reg.gaps, exp_auto_gaps)
 
-    def test_run_parallel(self):
+    def test_run_parallel(self, exp_parallel_gaps):
         reg = Region(self.points, self.bound, grid_size=0.05)
 
         reg.run_parallel(tile_size=0.8)
 
-        assert_geodataframe_equal(reg.gaps, self.exp_parallel_gaps)
+        assert_geodataframe_equal(reg.gaps, exp_parallel_gaps)
